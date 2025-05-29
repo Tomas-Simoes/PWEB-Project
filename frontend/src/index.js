9e855099d137b4b6f8bb7fdc96a61e18d4af5449
leorcf <leonardorcferreira@gmail.com>
2025-05-29 23:12:14 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 const form = document.getElementById('loginForm');
 
 form.addEventListener('submit', async (e) => {
+    e.preventDefault();
 
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
@@ -18,7 +19,25 @@ form.addEventListener('submit', async (e) => {
 
     if (res.ok && data.token) {
       sessionStorage.setItem('token', data.token);
+      sessionStorage.setItem('token', data.role);
       console.log("Login correto!");
+
+      switch (data.role) {
+        case 'technician':
+            window.location.href = '/public/techPage.html';
+            break;
+        case 'client':
+            window.location.href = '/public/clientPage.html';
+            break;
+        case 'operation manager':
+            window.location.href = '/public/energyMonitoring.html';
+            break;
+        default:
+            console.warn('Role desconhecida:', data.role);
+            window.location.href = '/pages/dashboard.html';
+            break;
+    }
+
     } else {
       console.log("falha no login")
     }
